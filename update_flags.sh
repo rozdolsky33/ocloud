88703004905e0c652c9e1a6570bf1ac58f46e837
Volodymyr Rozdolsky <rozdolskyvolodymyr@gmail.com>
2025-06-12 19:51:59 -0400

    chore: add placeholder comment to `update_flags.sh` script to trigger changes

--- update_flags.sh
+++ update_flags.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# trigger
 # Update all references to pkg/flags to internal/config
 find . -type f -name "*.go" -exec sed -i '' 's/github.com\/rozdolsky33\/ocloud\/pkg\/flags/github.com\/rozdolsky33\/ocloud\/internal\/config/g' {} \;
 find . -type f -name "*.go" -exec sed -i '' 's/flags\./config\./g' {} \;
