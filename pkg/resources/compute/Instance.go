ceab3c470240ae3f76b66cfbd4cc91e623101a6b
Volodymyr Rozdolsky <rozdolskyvolodymyr@gmail.com>
2025-06-13 16:50:46 -0400

    refactor: add Instance type and improve instance fetching with detailed mapping and OCI integration

--- pkg/resources/compute/Instance.go
+++ pkg/resources/compute/Instance.go
@@ -1,7 +1,10 @@
 package compute
 
 import (
+	"context"
 	"fmt"
+	"github.com/oracle/oci-go-sdk/v65/core"
+	"github.com/rozdolsky33/ocloud/internal/oci"
 
 	"github.com/rozdolsky33/ocloud/internal/app"
 	"github.com/rozdolsky33/ocloud/internal/logger"
@@ -9,16 +12,26 @@ import (
 
 // ListInstances lists all instances in the configured compartment using the provided application.
 // It uses the pre-initialized compute client from the AppContext struct.
-func ListInstances(application *app.AppContext) error {
+func ListInstances(appCtx *app.AppContext) error {
 	// Use VerboseInfo to ensure debug logs work with shorthand flags
-	logger.VerboseInfo(application.Logger, 1, "ListInstances()")
+	logger.VerboseInfo(appCtx.Logger, 1, "ListInstances()")
+	client, err := oci.NewComputeClient(appCtx.Provider)
+	if err != nil {
+		return err
+	}
+	ctx := context.Background()
+
+	instances, err := FetchInstances(ctx, client, appCtx)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("\nInstances:")
+	fmt.Println(instances)
 
 	// Use the pre-initialized compute client from the AppContext struct
 	// No need to create a new client
-
-	fmt.Println("Inside Instance resources running List Instances")
 	// Use application.ComputeClient to list instances
-	// ...
 
 	return nil
 }
@@ -41,4 +54,93 @@ func FindInstances(application *app.AppContext, namePattern string, showImageDet
 	// ...
 
 	return nil
+}
+
+func FetchInstances(ctx context.Context, computeClient core.ComputeClient, appCtx *app.AppContext) ([]Instance, error) {
+	// create a VNets client
+	networkClient, err := oci.NewNetworkClient(appCtx.Provider)
+	if err != nil {
+		return nil, err
+	}
+	var instances []Instance
+	page := ""
+	for {
+		resp, err := computeClient.ListInstances(ctx, core.ListInstancesRequest{
+			CompartmentId:  &appCtx.CompartmentID,
+			LifecycleState: core.InstanceLifecycleStateRunning,
+			Page:           &page,
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		for _, oc := range resp.Items {
+			inst := mapToInstance(oc)
+			fmt.Println()
+			instances = append(instances, inst)
+			// 1) get the VNIC attachment(s) for this instance
+			vaResp, err := computeClient.ListVnicAttachments(ctx, core.ListVnicAttachmentsRequest{
+				CompartmentId: &appCtx.CompartmentID,
+				InstanceId:    oc.Id,
+			})
+			if err != nil {
+				return nil, err
+			}
+			// 2) find the primary VNIC and call GetVnic
+			// after ListVnicAttachments into vaResp.Items...
+
+			for _, attachment := range vaResp.Items {
+				vnicResp, err := networkClient.GetVnic(ctx, core.GetVnicRequest{
+					VnicId: attachment.VnicId,
+				})
+				if err != nil {
+					return nil, err
+				}
+				// Now vnicResp.IsPrimary exists
+				if vnicResp.IsPrimary != nil && *vnicResp.IsPrimary {
+					inst.IP = *vnicResp.PrivateIp
+					inst.SubnetID = *vnicResp.SubnetId
+					break
+				}
+			}
+			instances = append(instances, inst)
+			fmt.Println("Name:", *oc.DisplayName)
+			fmt.Println("ID:", *oc.Id)
+			fmt.Println("Private IP:", inst.IP, "AD:", *oc.AvailabilityDomain, "\t", "FD:", *oc.FaultDomain, "\t", "Region:", *oc.Region)
+			fmt.Println("Shape:", *oc.Shape, "\t", "Memory:", *oc.ShapeConfig.MemoryInGBs, "GB", "\t vCPUs:", *oc.ShapeConfig.Vcpus)
+			fmt.Println("State:", oc.LifecycleState)
+			fmt.Println("Created:", oc.TimeCreated)
+			fmt.Println("Subnet ID: ", inst.SubnetID)
+
+		}
+
+		if resp.OpcNextPage == nil {
+			break
+		}
+		page = *resp.OpcNextPage
+	}
+	return instances, nil
+}
+
+// mapToInstance transforms the OCI SDK Instance into our local Instance type.
+func mapToInstance(oc core.Instance) Instance {
+	return Instance{
+		Name: *oc.DisplayName,
+		ID:   *oc.Id,
+		IP:   "", // to be filled later
+		Placement: Placement{
+			Region:             *oc.Region,
+			AvailabilityDomain: *oc.AvailabilityDomain,
+			FaultDomain:        *oc.FaultDomain,
+		},
+		Resources: Resources{
+			VCPUs:    *oc.ShapeConfig.Vcpus,
+			MemoryGB: *oc.ShapeConfig.MemoryInGBs,
+		},
+		ImageID:         *oc.ImageId,
+		SubnetID:        "", // to be filled later
+		State:           oc.LifecycleState,
+		CreatedAt:       *oc.TimeCreated,
+		OperatingSystem: "", // added per TODO
+	}
 }
---
+++ pkg/resources/compute/types.go
@@ -0,0 +1,33 @@
+package compute
+
+import (
+	"github.com/oracle/oci-go-sdk/v65/common"
+	"github.com/oracle/oci-go-sdk/v65/core"
+)
+
+// Instance represents a VM instance in the cloud.
+type Instance struct {
+	Name            string
+	ID              string
+	IP              string
+	ImageID         string
+	SubnetID        string
+	State           core.InstanceLifecycleStateEnum
+	CreatedAt       common.SDKTime
+	OperatingSystem string
+	Placement       Placement
+	Resources       Resources
+}
+
+// Placement groups region/availability/fault‐domain info.
+type Placement struct {
+	Region             string
+	AvailabilityDomain string
+	FaultDomain        string
+}
+
+// Resources group CPU and memory sizing.
+type Resources struct {
+	VCPUs    int
+	MemoryGB float32
+}
