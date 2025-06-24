4f340e66404e6c009549845cfa64a592d2ccde1a
Volodymyr Rozdolsky <rozdolskyvolodymyr@gmail.com>
2025-06-24 10:48:34 -0400

    refactor(identity): update flag references in compartment list command
    - Renamed import alias for better clarity.
    - Updated flag references to correctly use `compartmentFlags` instead of `instaceFlags`.
    - Added flag-specific logic to enhance list command functionality.

--- cmd/identity/compartment/list.go
+++ cmd/identity/compartment/list.go
@@ -1,7 +1,7 @@
 package compartment
 
 import (
-	instaceFlags "github.com/rozdolsky33/ocloud/cmd/compute/flags"
+	compartmentFlags "github.com/rozdolsky33/ocloud/cmd/compute/flags"
 	"github.com/rozdolsky33/ocloud/internal/app"
 	"github.com/rozdolsky33/ocloud/internal/config/flags"
 	"github.com/rozdolsky33/ocloud/internal/logger"
@@ -22,16 +22,21 @@ func NewListCmd(appCtx *app.ApplicationContext) *cobra.Command {
 			return RunListCommand(cmd, appCtx)
 		},
 	}
+	// Add flags specific to the list command
+	compartmentFlags.LimitFlag.Add(cmd)
+	compartmentFlags.PageFlag.Add(cmd)
+
 	return cmd
 
 }
 
 // RunListCommand handles the execution of the list command
 func RunListCommand(cmd *cobra.Command, appCtx *app.ApplicationContext) error {
-	useJSON := flags.GetBoolFlag(cmd, flags.FlagNameJSON, false)
 	// Get pagination parameters
-	limit := flags.GetIntFlag(cmd, flags.FlagNameLimit, instaceFlags.FlagDefaultLimit)
-	page := flags.GetIntFlag(cmd, flags.FlagNamePage, instaceFlags.FlagDefaultPage)
+	limit := flags.GetIntFlag(cmd, flags.FlagNameLimit, compartmentFlags.FlagDefaultLimit)
+	page := flags.GetIntFlag(cmd, flags.FlagNamePage, compartmentFlags.FlagDefaultPage)
+	useJSON := flags.GetBoolFlag(cmd, flags.FlagNameJSON, false)
+
 	// Use LogWithLevel to ensure debug logs work with shorthand flags
 	logger.LogWithLevel(logger.CmdLogger, 1, "Running compartment list command in", "compartment", appCtx.CompartmentName, "json", useJSON)
 	return compartment.ListCompartments(appCtx, useJSON, limit, page)
